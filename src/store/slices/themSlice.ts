5c47a875da65d400d20bad9b263894db6bdad216
Ahmed Al-Halawany <ahmedsamir.fcih2015@gmail.com>
2023-01-08 22:25:02 +0200

    add redux store and them slice

---
+++ src/store/slices/themSlice.ts
@@ -0,0 +1,29 @@
+import { createSlice, PayloadAction } from '@reduxjs/toolkit';
+import { Appearance, ColorSchemeName } from 'react-native';
+import { RootState } from 'store';
+
+type ThemeState = {
+  isDark: boolean;
+  theme: ColorSchemeName;
+};
+
+const slice = createSlice({
+  name: 'theme',
+  initialState: {
+    isDark: Appearance.getColorScheme() === 'dark',
+    theme: Appearance.getColorScheme(),
+  } as ThemeState,
+  reducers: {
+    setTheme: (state, { payload: theme }: PayloadAction<ColorSchemeName>) => {
+      state.isDark = theme === 'dark';
+      state.theme = theme;
+    },
+  },
+});
+
+export const { setTheme } = slice.actions;
+
+export default slice.reducer;
+
+export const selectTheme = (state: RootState) => state.theme.theme;
+export const selectIsDark = (state: RootState) => state.theme.isDark;
